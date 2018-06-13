c6fa7f2726681f98f0fbd6230bd05593d0d7d659
xxfcz <xxf_cz@163.com>
2018-06-13 16:59:03 +0800

    reorder links on home page

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -2,13 +2,13 @@
   <div>
     <ul>
       <li>
-        <router-link to="inspect">巡检</router-link>
-      </li>
-      <li>
         <router-link to="tasks">任务清单</router-link>
       </li>
       <li>
-        <router-link to="local-records">本地巡检记录</router-link>
+        <router-link to="inspect">巡检</router-link>
+      </li>
+      <li>
+        <router-link to="local-records">本地暂存的巡检记录</router-link>
       </li>
       <li>
         <router-link to="remote-records">服务器上的巡检记录</router-link>
