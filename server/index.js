a46fca35c88cb74302cdcf36346a2e2ca316093e
xxfcz <xxf_cz@163.com>
2018-06-19 21:46:49 +0800

    back to using json-server only

--- server/index.js
+++ server/index.js
@@ -1,16 +1,18 @@
-const express = require('express')
-let app = express()
+//const express = require('express')
+//let app = express()
 const path = require('path')
 const jsonServer = require('json-server')
-
+const app = jsonServer.create()
 
 const router = jsonServer.router(path.join(__dirname, 'db.json'))
 app.use('/api', router)
 
-app.use(express.static(path.join(__dirname, '../dist')))
+//app.use(express.static(path.join(__dirname, '../dist')))
 
-// const middlewares = jsonServer.defaults()
-// app.use(middlewares)
+const middlewares = jsonServer.defaults({
+  static: path.join(__dirname, '../dist')
+})
+app.use(middlewares)
 
 app.get('/echo', (req, res) => {
   res.send('hello!')
