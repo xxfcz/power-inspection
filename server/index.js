44339e53c22c109bd9040e327956748641ab8ef4
xxfcz <xxf_cz@163.com>
2018-06-20 11:36:26 +0800

    +) support for uploading multiple files

--- server/index.js
+++ server/index.js
@@ -8,9 +8,15 @@ const _ = require('lodash')
 
 //const app = jsonServer.create()
 app.use(express.static(path.join(__dirname, '../dist')))
+app.use('/upload', express.static(path.join(__dirname, './upload')))
+
+// app.post('/api/inspects', (req, res)=>{
+//   res.send('caught: POST /api/inspects');
+// })
 
 const router = jsonServer.router(path.join(__dirname, 'db.json'))
 app.use('/api', router)
+
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -24,14 +30,19 @@ app.get('/echo', (req, res) => {
 // 文件上传
 app.post('/upload', (req, res) => {
   var form = new formidable.IncomingForm()
-  form.uploadDir = path.join(__dirname, '../dist/upload')
+  form.uploadDir = path.join(__dirname, './upload')
   form.keepExtensions = true
   form.maxFileSize = 5 * 1024 * 1024
 
   form.parse(req, function(err, fields, files) {
     res.send(
       _.map(files, e => {
-        return { path: e.path.replace(form.uploadDir, 'http://192.168.0.103:3002/upload'), name: e.name }
+        return {
+          path: e.path.replace(form.uploadDir, '/upload'),
+          name: e.name,
+          type: e.type,
+          size: e.size
+        }
       })
     )
   })
