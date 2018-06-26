1b249341c1f9c2b6ddc171be745552bd147ce670
xxfcz <xxf_cz@163.com>
2018-06-26 23:05:48 +0800

    tests for sequelize.js

--- server/tests/sequelize_test.js
+++ server/tests/sequelize_test.js
@@ -1,4 +1,6 @@
 const Sequelize = require('sequelize')
+const Op = Sequelize.Op
+
 const sequelize = new Sequelize('powerins', 'postgres', 'postgres', {
   host: 'localhost',
   dialect: 'postgres',
@@ -22,26 +24,105 @@ const User = sequelize.define('user', {
   }
 })
 
-sequelize
-  .authenticate()
-  .then(() => {
-    console.log('Connection has been established successfully.')
+const Workshop = sequelize.define('workshop', {
+  name: {
+    type: Sequelize.STRING
+  }
+})
 
-    // force: true will drop the table if it already exists
-    User.sync({ force: true })
-      .then(() => {
-        // Table created
-        return User.create({
-          firstName: 'John',
-          lastName: 'Hancock'
-        })
+const Section = sequelize.define('section', {
+  name: {
+    type: Sequelize.STRING
+  }
+})
+
+Section.belongsTo(Workshop)
+
+var initDb = async () => {
+  try {
+    await Workshop.sync({ force: true })
+    await Section.sync({ force: true })
+
+    let names = ['衡阳供电车间', '耒阳供电车间', '郴州供电车间', '西渡供电车间']
+    for (let w of names) {
+      await Workshop.findOrCreate({
+        where: { name: w }
       })
-      .then(() => {
-        User.findOne().then(user => {
-          console.log(user.get('firstName'))
-        })
+    }
+
+    let w = await Workshop.findOne({
+      where: {
+        name: '衡阳供电车间'
+      }
+    })
+
+    for (let s of [
+      '衡阳至周家坳自闭',
+      '衡阳至周家坳贯通',
+      '周家坳至东阳渡自闭',
+      '周家坳至东阳渡贯通',
+      '东阳渡至向阳桥自闭',
+      '东阳渡至向阳桥贯通',
+      '哲桥至耒阳自闭',
+      '哲桥至耒阳贯通'
+    ]) {
+      await Section.findOrCreate({
+        where: {
+          name: s,
+          workshopId: w.id
+        }
       })
+    }
+  } catch (err) {
+    console.log('===============================================')
+    console.error('initDb(): Error occurred:', err)
+  }
+}
+
+var play = async () => {
+  let w1 = await Workshop.findOne({
+    where: {
+      name: '衡阳供电车间'
+    }
   })
-  .catch(err => {
-    console.error('Unable to connect to the database:', err)
+  //console.log(w1.get({ plain: true }))
+
+  await Section.update({
+    workshopId: 2
+  },{
+    where: {
+      id: { [Op.gte]: 7}
+    }
   })
+}
+
+var run = async () => {
+  try {
+    await sequelize.authenticate()
+    console.log('Connection has been established successfully.')
+    await initDb()
+
+    await play()
+  } catch (err) {
+    console.log('===============================================')
+    console.error('play(): Error occurred:', err)
+  } finally {
+    process.exit(0)
+  }
+}
+
+const Project = sequelize.define('project', {
+  name: Sequelize.STRING
+})
+
+Project.hasOne(User)
+
+var test = async () => {
+  await sequelize.authenticate()
+  await User.sync({ force: true })
+  await Project.sync({ force: true })
+  process.exit(0)
+}
+
+run()
+//test()
