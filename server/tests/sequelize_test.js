95ddf58c826d577b118133f51dc9b40f4d6b839d
xxfcz <xxf_cz@163.com>
2018-06-26 23:42:44 +0800

    tests for sequelize.js

--- server/tests/sequelize_test.js
+++ server/tests/sequelize_test.js
@@ -15,15 +15,6 @@ const sequelize = new Sequelize('powerins', 'postgres', 'postgres', {
 })
 // const sequelize = new Sequelize('postgres://user:pass@example.com:5432/dbname');
 
-const User = sequelize.define('user', {
-  firstName: {
-    type: Sequelize.STRING
-  },
-  lastName: {
-    type: Sequelize.STRING
-  }
-})
-
 const Workshop = sequelize.define('workshop', {
   name: {
     type: Sequelize.STRING
@@ -36,40 +27,58 @@ const Section = sequelize.define('section', {
   }
 })
 
+const User = sequelize.define('user', {
+  name: {
+    type: Sequelize.STRING
+  },
+  password: {
+    type: Sequelize.STRING
+  }
+})
+
 Section.belongsTo(Workshop)
+User.belongsTo(Workshop)
 
 var initDb = async () => {
   try {
-    await Workshop.sync({ force: true })
-    await Section.sync({ force: true })
+    await sequelize.sync({ force: true })
 
-    let names = ['衡阳供电车间', '耒阳供电车间', '郴州供电车间', '西渡供电车间']
-    for (let w of names) {
-      await Workshop.findOrCreate({
-        where: { name: w }
+    //await sequelize.getQueryInterface().bulkDelete('workshops')
+    // 添加车间
+    await Workshop.bulkCreate(
+      ['衡阳供电车间', '耒阳供电车间', '郴州供电车间', '西渡供电车间'].map(
+        e => {
+          return { name: e }
+        }
+      )
+    )
+
+    // 添加区段
+    await Section.bulkCreate(
+      [
+        ['衡阳至周家坳自闭', 1],
+        ['衡阳至周家坳贯通', 1],
+        ['周家坳至东阳渡自闭', 1],
+        ['周家坳至东阳渡贯通', 1],
+        ['东阳渡至向阳桥自闭', 1],
+        ['东阳渡至向阳桥贯通', 1],
+        ['哲桥至耒阳自闭', 2],
+        ['哲桥至耒阳贯通', 2]
+      ].map(e => {
+        return {
+          name: e[0],
+          workshopId: e[1]
+        }
       })
-    }
+    )
 
-    let w = await Workshop.findOne({
-      where: {
-        name: '衡阳供电车间'
-      }
-    })
-
-    for (let s of [
-      '衡阳至周家坳自闭',
-      '衡阳至周家坳贯通',
-      '周家坳至东阳渡自闭',
-      '周家坳至东阳渡贯通',
-      '东阳渡至向阳桥自闭',
-      '东阳渡至向阳桥贯通',
-      '哲桥至耒阳自闭',
-      '哲桥至耒阳贯通'
-    ]) {
-      await Section.findOrCreate({
+    // 添加账号
+    let users = [['肖雪峰', 4], ['张三', 1], ['李四', 2]]
+    for (let u of users) {
+      await User.findOrCreate({
         where: {
-          name: s,
-          workshopId: w.id
+          name: u[0],
+          workshopId: u[1]
         }
       })
     }
@@ -85,22 +94,15 @@ var play = async () => {
       name: '衡阳供电车间'
     }
   })
-  //console.log(w1.get({ plain: true }))
-
-  await Section.update({
-    workshopId: 2
-  },{
-    where: {
-      id: { [Op.gte]: 7}
-    }
-  })
+  let users = await w1.getUsers()
+  console.log(users.get({ plain: true }))
 }
 
 var run = async () => {
   try {
     await sequelize.authenticate()
     console.log('Connection has been established successfully.')
-    await initDb()
+    //await initDb()
 
     await play()
   } catch (err) {
