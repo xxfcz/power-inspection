a80a68b615f7d802605bb670d11ae1a1d2ffbfe5
xxfcz <xxf_cz@163.com>
2018-07-19 21:00:04 +0800

    axios增加响应拦截器，未登录时自动跳转到登录页面

--- src/main.js
+++ src/main.js
@@ -11,20 +11,42 @@ import VueMomentLib from 'vue-moment-lib'
 import bytes from 'bytes'
 import _ from 'lodash'
 
-
 // http request 请求拦截器，有token值则配置上token值
 axios.interceptors.request.use(
   config => {
-      let token = localStorage.getItem('token')
-      if (token) {  // 每次发送请求之前判断是否存在token，如果存在，则统一在http请求的header都加上token，不用每次请求都手动添加了
-          config.headers.Authorization = 'Bearer ' + token;
-      }
-      return config;
+    let token = localStorage.getItem('token')
+    if (token) {
+      // 每次发送请求之前判断是否存在token，如果存在，则统一在http请求的header都加上token，不用每次请求都手动添加了
+      config.headers.Authorization = 'Bearer ' + token
+    }
+    return config
   },
   err => {
-      return Promise.reject(err);
-  });
+    return Promise.reject(err)
+  }
+)
 
+// http response 服务器响应拦截器，这里拦截401错误，并重新跳入登页重新获取token
+axios.interceptors.response.use(
+  response => {
+    return response
+  },
+  error => {
+    if (error.response) {
+      switch (error.response.status) {
+        case 401:
+          // 清除token
+          localStorage.removeItem('user')
+          localStorage.removeItem('token')
+          router.replace({
+            path: '/login',
+            query: { redirect: router.currentRoute.fullPath } //登录成功后跳入浏览的当前页面
+          })
+      }
+    }
+    return Promise.reject(error.response.data)
+  }
+)
 
 Vue.use(VueMomentLib)
 Vue.prototype.$xutils = xutils
@@ -42,17 +64,24 @@ db.version(1).stores({
 Vue.prototype.$db = db
 Vue.prototype.$_ = _
 
-Vue.filter('datetime', x=>{
+Vue.filter('datetime', x => {
   return Vue.moment(x).format('YYYY-MM-DD HH:mm')
 })
 
-Vue.filter('bytes', (n)=>{
+Vue.filter('bytes', n => {
   return bytes(n)
 })
 Vue.prototype.$bytes = bytes
 
 // 故障类型
-Vue.prototype.$faults = ['悬挂异物', '瓷瓶破损', '扎线松脱', '接头放弧', '弧垂过大', '保险熔断']
+Vue.prototype.$faults = [
+  '悬挂异物',
+  '瓷瓶破损',
+  '扎线松脱',
+  '接头放弧',
+  '弧垂过大',
+  '保险熔断'
+]
 
 /* eslint-disable no-new */
 new Vue({
