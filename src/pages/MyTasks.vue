d4554011497cf6b2b20b6c94e411e529247ec432
xxfcz <xxf_cz@163.com>
2018-07-20 17:14:30 +0800

    下载任务清单后更新 finished 标志

--- src/pages/MyTasks.vue
+++ src/pages/MyTasks.vue
@@ -9,7 +9,7 @@
         <div>
           {{task.id}}.
           <span style="font-weight:bold">{{task.name}}</span>
-          <span v-if="task.finished">已检</span>
+          <span v-if="task.finished" style="color: red">已检</span>
         </div>
         <div style="margin-left:2em">
           坐标：{{task.longitude}},{{task.latitude}}<br> 区段：{{task.section.name}}
@@ -37,13 +37,21 @@ export default {
     if (navigator.onLine) {
       this.download()
     } else {
-      this.$db.tasks
-        .toArray(a => {
-          this.tasks = a
-          return this.$db.config.get('tasks.lastUpdateTime')
+      // 在数据库中标记已完成的任务
+      this.$db.inspects
+        .each(ins => {
+          this.$db.tasks.update(ins.id, { finished: true })
         })
-        .then(r => {
-          this.lastUpdateTime = new Date(r.value)
+        .then(() => {
+          // 加载任务表
+          this.$db.tasks.toArray(a => {
+            this.tasks = a
+          })
+        })
+        .then(() => {
+          return this.$db.config.get('tasks.lastUpdateTime').then(r => {
+            this.lastUpdateTime = new Date(r.value)
+          })
         })
         .catch(e => {
           alert(e)
@@ -56,41 +64,49 @@ export default {
     },
 
     download() {
-      if (!navigator.onLine) {
-        alert('您已离线，无法下载')
-        return
-      }
-      let url = `/api/schedules/user/${this.user.id}/todo`
-      this.$axios
-        .get(url)
-        .then(resp => {
-          let date = resp.data.date
-          if(!date || date>this.$moment().format('YYYY-MM-DD')){
-            alert('今天没有任务！')
-            return
-          }
-          let tasks = resp.data.devices
-          this.tasks = tasks
-          this.lastUpdateTime = new Date()
-          this.$db.tasks
-            .clear()
-            .then(() => {
-              return this.$db.tasks.bulkPut(tasks)
-            })
-            .then(r => {
-              return this.$db.config.put({
-                name: 'tasks.lastUpdateTime',
-                value: this.lastUpdateTime
+      return new Promise((resolve, reject) => {
+        if (!navigator.onLine) {
+          alert('您已离线，无法下载')
+          reject()
+          return
+        }
+
+        let tasks
+        let url = `/api/schedules/user/${this.user.id}/todo`
+        this.$axios
+          .get(url)
+          .then(resp => {
+            let date = resp.data.date
+            if (!date || date > this.$moment().format('YYYY-MM-DD')) {
+              alert('今天没有任务！')
+              resolve(false)
+              return
+            }
+            tasks = resp.data.devices
+            this.lastUpdateTime = new Date()
+            return this.$db.inspects.each(ins => {
+              let task = tasks.find(e => {
+                return e.id == ins.id
               })
+              if (task) task.finished = true
             })
-            .catch(e => {
-              alert('保存失败！原因是：' + e)
+          })
+          .then(() => {
+            this.tasks = tasks
+            return this.$db.tasks.clear().then(() => {
+              this.$db.tasks.bulkPut(tasks)
             })
-        })
-        .catch(function(response) {
-          console.error(response)
-          alert(response)
-        })
+          })
+          .then(() => {
+            return this.$db.config.put({
+              name: 'tasks.lastUpdateTime',
+              value: this.lastUpdateTime
+            })
+          })
+          .catch(e => {
+            alert('保存失败！原因是：' + e)
+          })
+      })
     }
   }
 }
